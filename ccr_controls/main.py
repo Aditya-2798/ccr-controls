c5f2ca99a84dfc5b7073d4f97af3bea3f570c35d
Aditya Khandelwal <adityaabc70@gmail.com>
2026-02-18 16:25:40 +0530

    Correcting validators and main files

---
+++ ccr_controls/main.py
@@ -0,0 +1,6 @@
+def run() -> None:
+    print("CCR Data Control Engine started.")
+
+
+if __name__ == "__main__":
+    run()
--- ccr_controls/validators.py
+++
@@ -1,6 +0,0 @@
-def run() -> None:
-    print("CCR Data Control Engine started.")
-
-
-if __name__ == "__main__":
-    run()
